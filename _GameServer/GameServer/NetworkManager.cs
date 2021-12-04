f0f9f9c61cf7f2b7f1c3eee622a1980f9afb6a5f
Zedahkai <94405336+Zedahkai@users.noreply.github.com>
2021-12-03 21:16:26 -0600

    Add files via upload

---
+++ _GameServer/GameServer/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+
+    public GameObject playerPrefab;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+
+        Server.Start(50, 21586);
+    }
+
+    private void OnApplicationQuit() => Server.Stop();
+
+    public Player InstantiatePlayer()
+    {
+        try
+        {
+
+            //Instantiate(playerPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity);
+            return Instantiate(playerPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity).GetComponent<Player>();
+        } catch (Exception _ex)
+        {
+            Debug.Log("Error creating player object");
+            return null;
+        }
+    }
+}
---
+++ _GameServer/GameServer/ServerHandle.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+
+public class ServerHandle
+{
+    public static void WelcomeReceived(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ReadString();
+
+        Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+        if (_fromClient != _clientIdCheck)
+        {
+            Debug.Log($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+        }
+
+
+    }
+
+    public static void PlayerMovement(int _fromClient, Packet _packet)
+    {
+        bool[] _inputs = new bool[_packet.ReadInt()];
+        for (int i = 0; i < _inputs.Length; i++)
+        {
+            _inputs[i] = _packet.ReadBool();
+        }
+        Quaternion _rotation = _packet.ReadQuaternion();
+
+        Server.clients[_fromClient].player.SetInput(_inputs, _rotation);
+    }
+
+    public static void LoginInfo(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ReadString();
+        string _password = _packet.ReadString();
+
+        using (var client = new System.Net.WebClient())
+        {
+            string _msg;
+            var values = new System.Collections.Specialized.NameValueCollection();
+            values.Add("username", _username);
+            values.Add("password", _password);
+            var response = client.UploadValues("http://localhost/sqlconnect/login.php", values);
+            var responseString = Encoding.ASCII.GetString(response);
+            if (responseString[0] == '0')
+            {
+                _msg = $"0 {_username} HAS LOGGED IN SUCCESSFULLY";
+                Debug.Log(_msg);
+                //Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+                Server.clients[_fromClient].SendIntoGame(_username);
+                ServerSend.LoginInfo(_fromClient, _msg);
+            }
+            else
+            {
+                _msg = $"LOGGIN FAILED: " + responseString;
+                Debug.Log(responseString);
+                ServerSend.LoginInfo(_fromClient, _msg);
+            }
+
+        }
+
+    }
+
+    public static void RegisterInfo(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ReadString();
+        string _password = _packet.ReadString();
+
+        using (var client = new System.Net.WebClient())
+        {
+            string _msg;
+            var values = new System.Collections.Specialized.NameValueCollection();
+            values.Add("username", _username);
+            values.Add("password", _password);
+            var response = client.UploadValues("http://localhost/sqlconnect/register.php", values);
+            var responseString = Encoding.ASCII.GetString(response);
+            if (responseString[0] == '0')
+            {
+                _msg = $"0 {_username} HAS CREATED AN ACCOUNT SUCCESSFULLY";
+                Debug.Log(_msg);
+                //Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+                // Server.clients[_fromClient].SendIntoGame(_username);
+                ServerSend.RegisterInfo(_fromClient, _msg);
+
+            }
+            else
+            {
+                _msg = "ACCOUNT CREATION FAILED " + responseString;
+                Debug.Log(_msg);
+                ServerSend.RegisterInfo(_fromClient, _msg);
+            }
+            //Server.clients[_fromClient].Disconnect();
+        }
+    }
+}
